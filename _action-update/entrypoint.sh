f9d934715482e4b1e7b29a52aa063141c273e79f
Faical Yannick Palingwende Congo <faical.congo@nist.gov>
2020-10-02 20:59:54 +0200

    Update entrypoint.sh

--- _action-update/entrypoint.sh
+++ _action-update/entrypoint.sh
@@ -9,12 +9,12 @@ hub version
 # Requires BRANCH_NAME, BOT_USER, BOT_TOKEN to be included by workflow
 export GITHUB_API_TOKEN=$BOT_TOKEN
 
-ACT_LOG_PATH=_explore/LAST_MASTER_UPDATE.txt
-ACT_INPUT_PATH=_explore
-ACT_DATA_PATH=explore/github-data
+#ACT_LOG_PATH=_explore/LAST_MASTER_UPDATE.txt
+#ACT_INPUT_PATH=_explore
+#ACT_DATA_PATH=explore/github-data
 
 DATA_TIMESTAMP=$(date -u "+%F-%H")
-CLONE_CUTOFF=$(date -u "+%F" -d "7 days ago")
+#CLONE_CUTOFF=$(date -u "+%F" -d "7 days ago")
 
 # Configure git + hub
 git config --global user.name "${BOT_USER}"
@@ -49,50 +49,6 @@ OLD_END=$(date --date="$OLD_END" "+%s")
 scraper --config $REPO_SCRAPER/nist_config.json
 
 git add -A .
-
-### VALIDATE UPDATE ###
-
-#   Timestamp log changed
-cat $ACT_LOG_PATH
-if [ $(git diff --name-only HEAD | grep -c "${ACT_LOG_PATH}") -ne "1" ]
-    then
-        echo "UPDATE FAILED - Timestamp log unchanged"
-        exit 1
-    else
-        echo "Timestamp log confirmed"
-fi
-
-#   Logged START and END without FAILED
-if [ $(cat $ACT_LOG_PATH | grep -c FAILED) -ne "0" ] || [ $(cat $ACT_LOG_PATH | grep -c START) -ne "1" ] || [ $(cat $ACT_LOG_PATH | grep -c END) -ne "1" ]
-    then
-        echo "UPDATE FAILED - Invalid timestamp log"
-        exit 1
-    else
-        echo "Timestamp log valid"
-fi
-
-#   New START is later than previous END
-NEW_START=$(cat $ACT_LOG_PATH | grep START | cut -f 2)
-NEW_START=$(date --date="$NEW_START" "+%s")
-if [ "$OLD_END" -gt "$NEW_START" ]
-    then
-        echo "UPDATE FAILED - New START is earlier than previous END"
-        exit 1
-    else
-        echo "START timestamp valid"
-fi
-
-#   All changes are to valid files only
-git diff --name-only HEAD
-CHANGE_COUNT=$(git diff --name-only HEAD | grep -c -E ".+")
-VALID_COUNT=$(git diff --name-only HEAD | grep -c -E "(^${ACT_DATA_PATH}\/\S+\.json$)|(^${ACT_INPUT_PATH}\/input\S+\.json$)|(${ACT_LOG_PATH})")
-if [ "$CHANGE_COUNT" -ne "$VALID_COUNT" ]
-    then
-        echo "UPDATE FAILED - Unexpected file changes"
-        exit 1
-    else
-        echo "Changed files validated"
-fi
 
 ### COMMIT UPDATE ###
 git pull
