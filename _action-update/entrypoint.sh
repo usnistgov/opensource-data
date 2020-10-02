303ef7bc29b28c279b7d11b1d37c02f968587b9e
Faical Yannick Palingwende Congo <faical.congo@nist.gov>
2020-10-02 22:56:22 +0200

    Update entrypoint.sh

--- _action-update/entrypoint.sh
+++ _action-update/entrypoint.sh
@@ -58,6 +58,8 @@ git push origin $BRANCH_NAME
 ### MERGE TO NIST-PAGES
 git checkout nist-pages
 git merge $BRANCH_NAME
+git add -A .
+git commit -m "${DATA_TIMESTAMP} Data Merge to ${BOT_USER}"
 git push origin nist-pages
 
 #push in
